8a3bb660cded0e48fb1ddded9c43a3f53285bad3
Alejandro Rial <alejandrorial01@gmail.com>
2023-09-20 17:07:22 +0200

    html + css

---
+++ js/function.js
@@ -0,0 +1,11 @@
+const form = document.querySelector('.loginForm');
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    if(document.querySelector('#user').value === "admin" && document.querySelector('#password').value === "admin"){
+    window.location.href = "../dashboard.html";
+    }
+    else {
+        alert("Para entrar: User: Admin, Contraseña: Admin")
+    }
+});
